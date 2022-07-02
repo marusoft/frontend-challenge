a4f8dab33b8ed2bef4bbd7924c354b6b1de2e4e8
Alimi Kehinde Morufudeen <alimi.kmaruf@gmail.com>
2022-07-02 17:26:48 +0100

    rename Dashboard

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const dashboard = () => {
+const Dashboard = () => {
   return (
-    <div>dashboard</div>
+    <div>Dashboard</div>
   )
 }
 
-export default dashboard
+export default Dashboard;
