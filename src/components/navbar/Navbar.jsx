24e74d49f6be59a2c26a8cbfe697de73a2fa5176
Alimi Kehinde Morufudeen <alimi.kmaruf@gmail.com>
2022-07-02 20:57:36 +0100

    update readme file

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,20 @@
 import React from "react";
-import Brand from "../../assets/brand/Union.png"
-import "./Navbar.scss"
+import Brand from "../../assets/brand/Union.png";
+import { BsSearch } from "react-icons/bs";
+import "./Navbar.scss";
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      <div className="logo">
-        <img src={Brand} alt="lendsqr-brand" />
-        <span className="logoText">lendsqr</span>
+      <div className="navbar-wrapper">
+        <div className="logo">
+          <img src={Brand} alt="lendsqr-brand" />
+          <span className="logoText">lendsqr</span>
+        </div>
+        <div className="search-bar">
+          <input type="text" placeholder="Search for anything" />
+          <button><BsSearch /></button>
+        </div>
       </div>
     </div>
   );
