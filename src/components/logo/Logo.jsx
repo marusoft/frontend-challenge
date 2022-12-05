64f4d5611c13a6e969a4db64431e4bf7ec448f1f
Alimi Kehinde Morufudeen <alimi.kmaruf@gmail.com>
2022-12-05 15:02:55 +0100

    refactor logo component

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Brand from "../../assets/brand/website_logo.png";
 import { LogoWrapper, LogoImage } from "./logo.styled";
 
-const Logo = ({pb}) => {
+const Logo = ({ pb }) => {
   return (
     <LogoWrapper pb={pb}>
       <LogoImage src={Brand} alt="lendsqr-brand" />
